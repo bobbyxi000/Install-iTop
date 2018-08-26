7a0b059656bf160ccbc7fbbfd4661761eb47d02e
Bobby F <40128726+bobbyxi000@users.noreply.github.com>
2018-08-27 02:02:19 +0800

    install itop 2.5

---
+++ install_itop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#!/usr/bin/env bash
+yum instlall unzip -y
+wget https://sourceforge.net/projects/itop/files/latest/download?source=files -O iTop-2.5.0-3935.zip
+unzip iTop-2.5.0-3935.zip
+mv web /var/www/html/itop
+chown -R apache:apache /var/www/html
