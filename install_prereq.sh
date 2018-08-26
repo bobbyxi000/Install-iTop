98afbbfaeeda922c6a3872cd3b87d77063f64f75
Bobby F <40128726+bobbyxi000@users.noreply.github.com>
2018-08-27 01:36:57 +0800

    Update install_prereq.sh

--- install_prereq.sh
+++ install_prereq.sh
@@ -2,4 +2,4 @@
 #!/usr/bin/env bash
 rpm -Uvh https://mirror.webtatic.com/yum/el7/epel-release.rpm
 rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-yum install -y php56w php56w-mcrypt php56w-opcach php56w-mysqlnd php56w-mcrypt php56w-xml php56w-cli php56w-soap php56w-ldap php56w-gd php56w-devel php56w-intl php56w-mbstring graphviz
+yum install -y php56w php56w-opcach php56w-mysqlnd php56w-mcrypt php56w-xml php56w-cli php56w-soap php56w-ldap php56w-gd php56w-devel php56w-intl php56w-mbstring graphviz
