ff03162c3d8950365984839177ca5ba6a11dd4eb
Bobby F <40128726+bobbyxi000@users.noreply.github.com>
2018-09-04 01:36:20 +0800

    Update install_itop.sh

--- install_itop.sh
+++ install_itop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #!/usr/bin/env bash
+# wget -O - 
 yum install -y unzip
 wget https://sourceforge.net/projects/itop/files/latest/download?source=files -O iTop-2.5.0-3935.zip
 unzip iTop-2.5.0-3935.zip
